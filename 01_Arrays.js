02dee5e6039ee12250462e9bf05a00797d06cd49
Hamza Khan <hamza.khan951hk@gmail.com>
2024-08-30 19:26:46 +0500

    Arrays_01

---
+++ 01_Arrays.js
@@ -0,0 +1,7 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5, 6];
+const myHeros = ["Iron man", "Loki", "GojoSatoru", "Eren", "Toji"];
+
+const myArr2 = new Array(1, 2, 3, 4);
+console.log(myArr[0]);
